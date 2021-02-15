049cbe3a2e32dd888c49ce3c2dcceaeec5336fc7
najuus <78907290+najuus@users.noreply.github.com>
2021-02-15 20:00:50 +0530

    movie portal

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+	<table class="table">
+		<tr>
+			<td>MOVIE NAME</td>
+			<td><input class="form-control" type="text"></td>
+		</tr>
+		<tr>
+			<td>ACTOR NAME</td>
+			<td><input class="form-control" type="text"></td>
+		</tr>
+		<tr>
+			<td>ACTRESS NAME</td>
+			<td><input class="form-control" type="text></td>
+		</tr>
+		<tr>
+			<td>DIRECTOR</td>
+			<td><input class="form-control" type="text"></td>
+		</tr>
+		<tr>
+			<td>PRODUCER</td>
+			<td><input class="form-control" type="text"></td>
+		</tr>
+		<tr>
+			<td>CAMERA</td>
+			<td><input class="form-control" type="text"></td>
+		</tr>
+		<tr>
+			<td>RELEASED YEAR</td>
+			<td><input class="form-control" type ="text"></td>
+		</tr>
+		<tr>
+			<td><button class="btn btn-danger"><b>SUBMIT</b>
+			</button></td>
+		</tr>
+	</table>
+</body>
+</html>
